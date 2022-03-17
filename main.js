d94c871b65f4bcaeb45aa2aade161aad0375d240
Léo Llorca <leo.llorca@hotmail.com>
2022-03-17 23:37:00 +0100

    Fonctions caractères et mots

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Let's start here !");
+const analyzeButton = document.querySelector('#analyzeButton');
+const analyzeInput = document.querySelector('#analyzeInput'); 
+
+analyzeButton.addEventListener('click', analyze);
+
+function analyze(){
+    const inputValue = analyzeInput.value;
+    const resultBox = document.querySelector('#result');
+    if (!inputValue) {
+        resultBox.innerHTML = "Veuillez remplir le champs ci-dessus narvalo.";
+        return;
+    }
+    const inputValueLength = inputValue.length;
+    const wordNumber = inputValue.split(' ').length;
+    resultBox.innerHTML = null;
+    const lengthTag = document.createElement("div");
+    const wordTag = document.createElement("div");
+    lengthTag.innerHTML = `Nombre de caractère: ${inputValueLength}`;
+    wordTag.innerHTML = `Nombre de mot: ${wordNumber}`;
+    resultBox.appendChild(lengthTag);
+    resultBox.appendChild(wordTag);
+}
